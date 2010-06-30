7c07682234bb15f19b3f5998415ee012d922fefd
ssp <ssp-web@earthlingsoft.net>
2010-06-30 19:56:56 +0200

    Change some copyright numbers.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -5,7 +5,7 @@
   Created by Tom Insam on 24/11/2008.
  
   
-  Copyright (c) 2009 Sven-S. Porst, Tom Insam
+  Copyright (c) 2009-2010 Sven-S. Porst, Tom Insam
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
